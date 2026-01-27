3ef9aa174c44e40680c662d3179e37cfdbfaa3bd
Bas Schoenmaeckers <b.schoenmaeckers@zanders.eu>
2026-01-27 16:35:41 +0100

    Add support for `wasip3`

--- src/tz_wasm32_wasi.rs
+++ src/tz_wasm32_wasi.rs
@@ -1,3 +1,11 @@
 pub(crate) fn get_timezone_inner() -> Result<String, crate::GetTimezoneError> {
-    std::env::var("TZ").or_else(|_| Ok("Etc/UTC".to_owned()))
+    #[cfg(any(target_env = "p1", target_env = "p2"))]
+    {
+        std::env::var("TZ").or_else(|_| Ok("Etc/UTC".to_owned()))
+    }
+
+    #[cfg(target_env = "p3")]
+    {
+        wasip3::clocks::timezone::iana_id().ok_or_else(|| crate::GetTimezoneError::OsError);
+    }
 }
